342b20b7b67ec5a60ccab53c34cd07173f112847
Devin Singh <drsingh2518@icloud.com>
2021-04-15 15:47:29 -0500

    refactor: simplify hilbilinestate

--- hilbiline.go
+++ hilbiline.go
@@ -10,42 +10,60 @@ import (
 )
 
 const (
+	// Keycodes
 	KeyNull      = 0
+	KeyCtrlA     = 1
+	KeyCtrlB     = 2
+	KeyCtrlC     = 3
 	KeyCtrlD     = 4
+	KeyCtrlE     = 5
+	KeyCtrlF     = 6
+	KeyCtrlH     = 8
 	KeyTab       = 9
+	KeyCtrlK     = 11
+	KeyCtrlL     = 12
 	KeyEnter     = 13
+	KeyCtrlN     = 14
+	KeyCtrlP     = 16
+	KeyCtrlT     = 20
+	KeyCtrlU     = 21
+	KeyCtrlW     = 23
 	KeyEsc       = 27
 	KeyBackspace = 127
 )
 
+var (
+	maskedmode = 0
+	mlmode     = 0
+)
+
 type HilbilineState struct {
-	// Line state
-	point int
-	end   int
-	mark  int
-	// buflen int (not needed?)
+	// io readers
+	stdio  *bufio.Reader
+	stdout *bufio.Reader
+
+	// Readline buffer and prompt
 	buf    []rune
 	prompt string
 
-	// Global state
-	rlstate int
-	done    int
-	// keymap Keymap
+	historyindex int
 
-	// Input state
-	// kseq *rune ???
-	// kseqlen int ???
+	pos    int // Current cursor position
+	oldpos int // Previous cursor position
+	cols   int // Num of terminal columns
 
-	// pendingin int ???
+	// Don't know if needed
+	// Num of rows in mlmode
+	maxrows int
 
 	r *bufio.Reader
 }
 
-func New(prompt string) HilbilineState {
+func NewHilbilineState(prompt string) HilbilineState {
 	return HilbilineState{
-		point:  0,
-		end:    0,
-		mark:   0,
+		stdio:  bufio.NewReader(os.Stdin),
+		stdout: bufio.NewReader(os.Stdout),
+
 		buf:    []rune{},
 		prompt: prompt,
 
