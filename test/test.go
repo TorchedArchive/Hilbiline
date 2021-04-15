9c3d01549e888656e1c960f28dbedc332aacb905
Devin Singh <drsingh2518@icloud.com>
2021-04-15 16:01:30 -0500

    fix: incremental testing

--- test/test.go
+++ test/test.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"io"
 	"hilbiline"
-	"fmt"
+	"os"
 )
 
 func main() {
-	hl := hilbiline.New("& ")
-	for {
-		str, e := hl.Read()
-		if e == io.EOF {
-			fmt.Println("hit ctrl d")
-			return
-		}
-
-		if e != nil {
-			panic(e)
-		}
-
-		fmt.Println(str)
-	}
+	homedir, _ := os.UserHomeDir()
+	defaultconfpath := homedir + "/.hilbish-history"
+	hl := hilbiline.NewHilbilineState("& ")
+	hl.AddHistFile(defaultconfpath)
+	// for {
+	// str, e := hl.Read()
+	// if e == io.EOF {
+	// fmt.Println("hit ctrl d")
+	// return
+	// }
+	//
+	// if e != nil {
+	// panic(e)
+	// }
+	//
+	// fmt.Println(str)
+	// }
 }
